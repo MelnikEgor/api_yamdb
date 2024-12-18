0d7311dd4b55f99b294e7677a49f29760e9b068c
Ruslan Musifullin <ruslan-magnat12@ya.ru>
2024-12-18 21:07:51 +0300

    Добавлены модели, сериализаторы, представления и эндпоинты

--- api_yamdb/api/views.py
+++ api_yamdb/api/views.py
@@ -1,3 +1,38 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAdminUser, AllowAny
+from rest_framework.filters import SearchFilter
+from django_filters.rest_framework import DjangoFilterBackend
+from reviews.models import Category, Genre, Title
+from .serializers import CategorySerializer, GenreSerializer, TitleSerializer
 
-# Create your views here.
+
+class BaseModelViewSet(viewsets.ModelViewSet):
+    filter_backends = []
+    search_fields = []
+    permission_classes = []
+
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            return [AllowAny()]
+        return [IsAdminUser()]
+
+
+class CategoryViewSet(BaseModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['name']
+
+
+class GenreViewSet(BaseModelViewSet):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    filter_backends = [SearchFilter]
+    search_fields = ['name']
+
+
+class TitleViewSet(BaseModelViewSet):
+    queryset = Title.objects.all()
+    serializer_class = TitleSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['category__slug', 'genre__slug', 'name', 'year']
