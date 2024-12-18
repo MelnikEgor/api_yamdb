12c9488163c39efe54f49dc7b7ed038f3e2642e2
Egor Melnik <m3lnikegor@gmail.com>
2024-12-18 07:30:32 +0300

    Исправление стандартных полей юзера, под наши задачи.

--- api_yamdb/users/models.py
+++ api_yamdb/users/models.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 
 class MyUser(AbstractUser):
+    password = models.CharField('password', max_length=128, blank=True)
+    email = models.EmailField(_('email address'), unique=True)
     bio = models.TextField('Биография', blank=True)
     role = models.TextField('Роль', blank=True)
